0465b83d496d03736694657261c7c30ee6ed1148
gomboad <gomboad@student.uzlabina.cz>
2024-03-13 01:20:58 +0100

    heading

--- README.md
+++ README.md
@@ -34,3 +34,35 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+import React from "react";
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+
+import ReactPDF from '@react-pdf/renderer';
+
+
+export default function Poster() {
+  return (
+    <div style={{
+        width: '297mm',
+        height: '420mm',
+        backgroundImage: `url('/yo2.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }}>
+      <div className="flex flex-col items-center text-zinc-300 pt-16 w-full">
+        <p className="text-7xl font-bold">Solwo</p>  
+      </div>  
+    </div>
+  );
+}
+
+const MyDocument = () => (
+  <Document>
+    <Page size="A3">
+      <Poster />
+    </Page>
+  </Document>
+);
+ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
-import Image from "next/image";
+'use client'
+import React, { useRef, useCallback } from "react";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 
-export default function Home() {
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import DoneAllRoundedIcon from '@mui/icons-material/DoneAllRounded';
+
+export default function Page() {
+  const posterRef = useRef(null);
+
+  const exportToPdf = useCallback(() => {
+    if (posterRef.current !== null) {
+      const printDPI = 300;
+      const scale = printDPI / 96;
+      html2canvas(posterRef.current, { scale: scale }).then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        
+        // A3 size is 297mm x 420mm.
+        const pdf = new jsPDF('p', 'mm', 'a3');
+        pdf.addImage(imgData, 'PNG', 0, 0, 297, 420);
+        pdf.save("poster.pdf");
+      });
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col gap-2 pb-2">
+      <Poster ref={posterRef} />
+      <button onClick={exportToPdf} className="text-2xl font-bold">Export to PDF</button>
+    </div>
+  );
+}
+
+const Poster = React.forwardRef<HTMLDivElement>((props, ref) => (
+  <div ref={ref} style={{
+      width: '297mm',
+      height: '420mm',
+      backgroundImage: `url('/yo2.png')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      color: 'white',
+  }}>
+    <div className="flex flex-col gap-10 items-center text-zinc-300 pt-16 px-10 w-full">
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <DoneAllRoundedIcon sx={{fontSize: 64}}/>
+            <p className="text-7xl font-bold">Solwo</p>
+          </div>
+          <p className="text-3xl font-semibold">Systém pro zadávání ticketů</p>
+        </div>
+        <div className="flex gap-3 pt-3">
+          <div className="flex flex-col items-end text-2xl gap-1">
+            <p>SPŠEaG</p>
+            <p>V Úžlabině</p>
+          </div>
+        </div>
+      </div>
+      
+
+      <div className="flex gap-3 text-3xl">
+        <div className="flex flex-col gap-3">
+          <p>Autor projektu:</p>
+          <p>Vedoucí práce:</p>
+        </div>
+        <div className="flex flex-col font-semibold gap-3">
+          <p>Adam Gombos</p>
+          <p>Sergey Kuroedov</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-3 items-center">
+          <ArrowForwardIosRoundedIcon fontSize="medium"/>
+          <p className="text-3xl">Coding</p>
+        </div>
+        <div className="flex gap-3 items-center">
+          <ArrowForwardIosRoundedIcon fontSize="medium"/>
+          <p className="text-3xl">More coding</p>
+        </div>
+        <div className="flex gap-3 items-center">
+          <ArrowForwardIosRoundedIcon fontSize="medium"/>
+          <p className="text-3xl">Even more coding</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+      
+    </div> 
+  </div>
+));
